4d97901e2e8807606acafda919e2020def62d48d
ej48 <ej48@duke.edu>
2015-04-26 16:26:29 -0400

    classification on a test set

--- digits.py
+++ digits.py
@@ -5,13 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import offsetbox
 from sklearn import (manifold, datasets, decomposition, ensemble, lda,
-                     random_projection)
+                     random_projection, linear_model, metrics)
 
 digits = datasets.load_digits(n_class=6)
-X = digits.data
-y = digits.target
-n_samples, n_features = X.shape
+n_samples, n_features = digits.data.shape
 n_neighbors = 30
+dimensions = 2
+training_size = int(0.8*n_samples)
+
+# training set
+X = digits.data[:training_size] 
+y = digits.target[:training_size]
+
+# test set
+X_test = digits.data[training_size:]
+y_test = digits.target[training_size:]
 
 
 #----------------------------------------------------------------------
@@ -30,7 +38,7 @@ def plot_embedding(X, title=None):
     if hasattr(offsetbox, 'AnnotationBbox'):
         # only print thumbnails with matplotlib > 1.0
         shown_images = np.array([[1., 1.]])  # just something big
-        for i in range(digits.data.shape[0]):
+        for i in range(X.shape[0]):
             dist = np.sum((X[i] - shown_images) ** 2, 1)
             if np.min(dist) < 4e-3:
                 # don't show points that are too close
@@ -62,48 +70,91 @@ plt.title('A selection from the 64-dimensional digits dataset')
 
 
 #----------------------------------------------------------------------
-# Projection on to the first 2 principal components
+# Projection on to the first dimensions principal components
 
 print("Computing PCA projection")
 t0 = time()
-X_pca = decomposition.TruncatedSVD(n_components=2).fit_transform(X)
+clf = decomposition.TruncatedSVD(n_components=dimensions)
+X_pca = clf.fit_transform(X)
+t1 = time()
 plot_embedding(X_pca,
                "Principal Components projection of the digits (time %.2fs)" %
-               (time() - t0))
-
+               (t1 - t0))
+               
+# Classification               
+log_clf = linear_model.LogisticRegression()
+log_clf.fit(X_pca, y)
+X_pca_test = clf.transform(X_test)
+y_pca_pred = log_clf.predict(X_pca_test)
 
 #----------------------------------------------------------------------
 # Isomap projection of the digits dataset
 print("Computing Isomap embedding")
 t0 = time()
-X_iso = manifold.Isomap(n_neighbors, n_components=2).fit_transform(X)
+clf = manifold.Isomap(n_neighbors, n_components=dimensions)
+X_iso = clf.fit_transform(X)
+t1 = time()
 print("Done.")
 plot_embedding(X_iso,
                "Isomap projection of the digits (time %.2fs)" %
-               (time() - t0))
+               (t1 - t0))
 
+# Classification               
+log_clf = linear_model.LogisticRegression()
+log_clf.fit(X_iso, y)
+X_iso_test = clf.transform(X_test)
+y_iso_pred = log_clf.predict(X_iso_test)
 
 #----------------------------------------------------------------------
 # Locally linear embedding of the digits dataset
 print("Computing LLE embedding")
-clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=2,
-                                      method='standard')
 t0 = time()
+clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=dimensions,
+                                      method='standard')
 X_lle = clf.fit_transform(X)
+t1 = time()
 print("Done. Reconstruction error: %g" % clf.reconstruction_error_)
 plot_embedding(X_lle,
                "Locally Linear Embedding of the digits (time %.2fs)" %
-               (time() - t0))
+               (t1 - t0))
+               
+# Classification               
+log_clf = linear_model.LogisticRegression()
+log_clf.fit(X_lle, y)
+X_lle_test = clf.transform(X_test)
+y_lle_pred = log_clf.predict(X_lle_test)
 
 #----------------------------------------------------------------------
 # MDS  embedding of the digits dataset
 print("Computing MDS embedding")
-clf = manifold.MDS(n_components=2, n_init=1, max_iter=100)
 t0 = time()
+clf = manifold.MDS(n_components=dimensions, n_init=1, max_iter=100)
 X_mds = clf.fit_transform(X)
+t1 = time()
 print("Done. Stress: %f" % clf.stress_)
 plot_embedding(X_mds,
                "MDS embedding of the digits (time %.2fs)" %
-               (time() - t0))
+               (t1 - t0))
+
+# Classification               
+log_clf = linear_model.LogisticRegression()
+log_clf.fit(X_mds, y)
+#X_mds_test = clf.transform(X_test)
+#y_mds_pred = log_clf.predict(X_mds_test)
+y_mds_pred = y_test
+
+#----------------------------------------------------------------------
+# Display results
+
+# baseline, fit on high dimension
+log_clf = linear_model.LogisticRegression()
+log_clf.fit(X, y)
+y_pred = log_clf.predict(X_test)
+print "Accuracy score for baseline: {0}".format(metrics.accuracy_score(y_pred, y_test))
+
+print "Accuracy score for PCA: {0}".format(metrics.accuracy_score(y_pca_pred, y_test))
+print "Accuracy score for Isomap: {0}".format(metrics.accuracy_score(y_iso_pred, y_test))
+print "Accuracy score for LLE: {0}".format(metrics.accuracy_score(y_lle_pred, y_test))
+print "Accuracy score for MDS: {0}".format(metrics.accuracy_score(y_mds_pred, y_test))
 
 plt.show()
