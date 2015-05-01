b5120cd992ea0bfcc67243dd0f9d79b04f3e4a67
ej48 <ej48@duke.edu>
2015-04-30 22:54:39 -0400

    code clean up

--- digits.py
+++ digits.py
@@ -6,21 +6,16 @@ import matplotlib.pyplot as plt
 from matplotlib import offsetbox
 from sklearn import (manifold, datasets, decomposition, ensemble, lda,
                      random_projection, linear_model, metrics)
-
-digits = datasets.load_digits(n_class=6)
-n_samples, n_features = digits.data.shape
+# dim reduction parameters
 n_neighbors = 30
 dimensions = 2              ### change this number
-X = digits.data
-y = digits.target
 
-# baseline, fit on high dimension
-log_clf = linear_model.LogisticRegression()
-log_clf.fit(X, y)
-y_pred = log_clf.predict(X)
-print "Accuracy score for baseline: {0}".format(metrics.accuracy_score(y_pred, y))
-
-quit()
+# load data
+X = np.genfromtxt('100examples.txt', delimiter=',')
+y = []
+for i in xrange(10):
+    for _ in xrange(100):
+        y.append(i)
 
 #----------------------------------------------------------------------
 # Scale and visualize the embedding vectors
@@ -31,7 +26,7 @@ def plot_embedding(X, title=None):
     plt.figure()
     ax = plt.subplot(111)
     for i in range(X.shape[0]):
-        plt.text(X[i, 0], X[i, 1], str(digits.target[i]),
+        plt.text(X[i, 0], X[i, 1], str(y[i]),
                  color=plt.cm.Set1(y[i] / 10.),
                  fontdict={'weight': 'bold', 'size': 9})
 
@@ -44,42 +39,17 @@ def plot_embedding(X, title=None):
                 # don't show points that are too close
                 continue
             shown_images = np.r_[shown_images, [X[i]]]
-            imagebox = offsetbox.AnnotationBbox(
-                offsetbox.OffsetImage(digits.images[i], cmap=plt.cm.gray_r),
-                X[i])
-            ax.add_artist(imagebox)
     plt.xticks([]), plt.yticks([])
     if title is not None:
         plt.title(title)
 
-
 #----------------------------------------------------------------------
-# Plot images of the digits
-n_img_per_row = 20
-img = np.zeros((10 * n_img_per_row, 10 * n_img_per_row))
-for i in range(n_img_per_row):
-    ix = 10 * i + 1
-    for j in range(n_img_per_row):
-        iy = 10 * j + 1
-        img[ix:ix + 8, iy:iy + 8] = X[i * n_img_per_row + j].reshape((8, 8))
-
-plt.imshow(img, cmap=plt.cm.binary)
-plt.xticks([])
-plt.yticks([])
-plt.title('A selection from the 64-dimensional digits dataset')
-
-
-#----------------------------------------------------------------------
-# Projection on to the first dimensions principal components
-
+# Projection on to the first d principal components
 print("Computing PCA projection")
-t0 = time()
 clf = decomposition.TruncatedSVD(n_components=dimensions)
 X_pca = clf.fit_transform(X)
-t1 = time()
 plot_embedding(X_pca,
-               "Principal Components projection of the digits (time %.2fs)" %
-               (t1 - t0))
+               "Principal Components projection of the digits")
                
 # Classification               
 log_clf = linear_model.LogisticRegression()
@@ -89,14 +59,10 @@ y_pca_pred = log_clf.predict(X_pca)
 #----------------------------------------------------------------------
 # Isomap projection of the digits dataset
 print("Computing Isomap embedding")
-t0 = time()
 clf = manifold.Isomap(n_neighbors, n_components=dimensions)
 X_iso = clf.fit_transform(X)
-t1 = time()
-print("Done.")
 plot_embedding(X_iso,
-               "Isomap projection of the digits (time %.2fs)" %
-               (t1 - t0))
+               "Isomap projection of the digits")
 
 # Classification               
 log_clf = linear_model.LogisticRegression()
@@ -106,15 +72,11 @@ y_iso_pred = log_clf.predict(X_iso)
 #----------------------------------------------------------------------
 # Locally linear embedding of the digits dataset
 print("Computing LLE embedding")
-t0 = time()
 clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=dimensions,
                                       method='standard')
 X_lle = clf.fit_transform(X)
-t1 = time()
-print("Done. Reconstruction error: %g" % clf.reconstruction_error_)
 plot_embedding(X_lle,
-               "Locally Linear Embedding of the digits (time %.2fs)" %
-               (t1 - t0))
+               "Locally Linear Embedding of the digits")
                
 # Classification               
 log_clf = linear_model.LogisticRegression()
@@ -122,26 +84,9 @@ log_clf.fit(X_lle, y)
 y_lle_pred = log_clf.predict(X_lle)
 
 #----------------------------------------------------------------------
-# MDS  embedding of the digits dataset
-print("Computing MDS embedding")
-t0 = time()
-clf = manifold.MDS(n_components=dimensions, n_init=1, max_iter=100)
-X_mds = clf.fit_transform(X)
-t1 = time()
-print("Done. Stress: %f" % clf.stress_)
-plot_embedding(X_mds,
-               "MDS embedding of the digits (time %.2fs)" %
-               (t1 - t0))
-
-# Classification               
-log_clf = linear_model.LogisticRegression()
-log_clf.fit(X_mds, y)
-y_mds_pred = log_clf.predict(X_mds)
-
-#----------------------------------------------------------------------
 # Display results
 
-# baseline, fit on high dimension
+# baseline, fit on high dimension (unreduced data)
 log_clf = linear_model.LogisticRegression()
 log_clf.fit(X, y)
 y_pred = log_clf.predict(X)
@@ -150,6 +95,5 @@ print "Accuracy score for baseline: {0}".format(metrics.accuracy_score(y_pred, y
 print "Accuracy score for PCA: {0}".format(metrics.accuracy_score(y_pca_pred, y))
 print "Accuracy score for Isomap: {0}".format(metrics.accuracy_score(y_iso_pred, y))
 print "Accuracy score for LLE: {0}".format(metrics.accuracy_score(y_lle_pred, y))
-print "Accuracy score for MDS: {0}".format(metrics.accuracy_score(y_mds_pred, y))
 
 plt.show()
